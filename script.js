1595c2e40268c391a5914d4f139dada88f127d4f
Arvin Miguel Antonio <irshadow08@gmail.com>
2024-01-19 09:14:43 +0800

    10% done

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-window.onload = function(){ document.getElementById("loading").style.display = "none" }
+window.onload = function() 
+{
+    var loadingElement = document.getElementById("loading");
+    loadingElement.classList.add("hidden");
+
+    // Removes the "hidden" after loading disappears
+    setTimeout(function() {
+        loadingElement.style.display = "none";
+    }, 1000);
+};
