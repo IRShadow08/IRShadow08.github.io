3c2aaede11129bfcee8c2eadd737e7e68ce9ab29
Arvin Miguel Antonio <irshadow08@gmail.com>
2024-01-20 02:04:07 +0800

    Almost Done

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ window.onload = function()
     }, 1000);
 };
 
+document.addEventListener('DOMContentLoaded', function () {
+    var aboutSection = document.querySelector('.about');
+    
+    var observer = new IntersectionObserver(function (entries, observer) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          aboutSection.classList.add('show');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, { threshold: 0.5 });
+  
+    observer.observe(aboutSection);
+  });
+
+function copyToClipboard() {
+    var textToCopy = document.getElementById("discordId").textContent;
+    var textarea = document.createElement("textarea");
+    textarea.value = textToCopy;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+    alert("Discord ID copied to clipboard!");
+}
